10cce11fab0417b3572845b6e154a1ed74b80700
Khashayar (Kay) <khashabri@gmail.com>
2024-10-14 20:15:46 +0200

    Many changes to make output pipeline friendly

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import yfinance as yf
 import numpy as np
-from tabulate import tabulate
 import os
 import sys
 from progress.bar import IncrementalBar
 import psutil
-from symbols_dataset import *
 import time
 import datetime
+from symbols_dataset import *
 
-########################### Paramters ###########################
+########################### Parameters ###########################
+last_week = datetime.datetime.now() - datetime.timedelta(days=7)
 
-start_date = "2023-01-01";
-end_date = None; # None or e.g. "2020-12-06"
+# Different date ranges and company lists with keys to iterate over
+date_configs = [
+    {"start_date": last_week.strftime("%Y-%m-%d"), "end_date": None}, # one week ago result
+    {"start_date": datetime.datetime(datetime.datetime.now().year - 1, 1, 1).strftime("%Y-%m-%d"), "end_date": None} # last year to now
+]
 
-# List of company tickers
-companies = [
+company_configs = [
+    {"key": "Big Tech", "companies": [
     "AAPL", "AXP", "BA", "CAT", "CSCO", "CVX", "DIS", "DOW", "GS", "HD", "IBM", "INTC",
     "JNJ", "JPM", "KO", "MCD", "MMM", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH",
     "RTX", "V", "VZ", "WBA", "WMT", "XOM", "AMZN", "GOOGL", "META", "BRK.AX", "NVDA"
-] # or e.g. companies = us_market
+    ]},    
+    {"key": "NASDAQ", "companies": us_market},
+    {"key": "DAX", "companies": de_market}
+]
 
 ############################ Helpers #############################
 class Timer(object):
@@ -30,9 +36,6 @@ class Timer(object):
         self.tstart = time.time()
 
     def stop(self):
-        if self.name:
-            print('[%s]' % self.name,)
-        # print('Elapsed: %s' % (time.time() - self.tstart))
         return time.time() - self.tstart
 
 
@@ -44,16 +47,14 @@ def printTime(remainedTime):
 def evaluate(rel_num_of_pos, mean_rel_growth, total_rel_growth):
     return ((1/3) * rel_num_of_pos + (1/3) * mean_rel_growth + (1/3) * total_rel_growth) * 10
 
-# Function to print the list of companies with their evaluated metrics
-def print_the_list(this_list):
+# Function to print and return the markdown table for a specific company list
+def print_the_list(this_list, start_date, end_date, key):
     headers = [ "Rank", "ID", "Company", "Evaluation Point", "Ratio Pos. Days", "Netto Growth", 
     "Mean Rel. Daily Growth", "Tot. Growth", "Current Price", "Sector"]
 
-    os.system('clear')  # Clear the terminal screen
     mytable = []
     rank = 1
     for i in range(len(this_list)):
-        # Append each company's data in a formatted row
         mytable.append([
             "{:}".format(rank), 
             this_list[i][0], 
@@ -68,108 +69,118 @@ def print_the_list(this_list):
         ])
         rank += 1
     
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get current time
-    # Construct the markdown table header
-    table_str = f"Last Run: {current_time}\n\n| " + " | ".join(headers) + " |\n"
+    table_str = f"## Results for {key} - From {start_date} until {end_date if end_date else 'Today'}\n\n"
+    table_str += f"| " + " | ".join(headers) + " |\n"
     table_str += "| " + " | ".join(['---' for _ in headers]) + " |\n"
 
-    # Add rows to the markdown table
     for row in mytable:
         table_str += "| " + " | ".join(row) + " |\n"
 
-    table_str += f"\n{start_date} - {end_date}"  # Add the date range to the output
+    return table_str
 
-    # Save in the same directory
-    output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "RESULT.md")
 
-    with open(output_file_path, "w") as file:
-        file.write(table_str)
-
-    print(table_str)
-
-# Initialize necessary lists and variables
-list_of_all_catched_data = []
+# Initialize necessary variables
 error_list = []
 myTimer = Timer()
-companies = list(dict.fromkeys(companies)) # Remove duplicate companies
-nmb_of_comp = len(companies)
-remained_nmb_of_comp = nmb_of_comp
 
-os.system('clear')  # Clear the terminal screen
-IncrementalBar = IncrementalBar('Processing', max=nmb_of_comp)  # Initialize a progress bar
+# Path to the output markdown file
+output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "RESULTS.md")
 
-# Loop through each company to download and process its data
-for company in companies:
-    myTimer.start()  # Start the timer for the company
-    try:
-        sys.stdout = open(os.devnull, "w")  # Suppress stdout
-        data = yf.download(
-            tickers=company,
-            start=start_date,
-            end=end_date,
-            interval="1d",
-            group_by='ticker'
-        )
-        
-        # Process the downloaded data
-        x = data["Adj Close"]
-        x = x[np.logical_not(np.isnan(x))]
-        x = np.array(x)
+# Delete the previous RESULTS.md file if it exists
+if os.path.exists(output_file_path):
+    os.remove(output_file_path)
 
-        # Calculate some metrics
-        diffs = np.concatenate(([0], np.diff(x)))           # daily price difference 
-        num_of_pos = np.count_nonzero(diffs > 0)            # number of bullish days
-        nettoGrowth = x[-1] - x[0]
-        rel_num_of_pos = num_of_pos / len(diffs)            # relative number of bullish days to total number of days
-        mean_rel_growth = np.mean(diffs[1:] / x[:-1])       # difference of closed price btw two days devided by closed of first day
-        total_rel_growth = nettoGrowth / x[0]               # total relative change
+# Open the output file in write mode
+with open(output_file_path, "w") as file:
+    # Write the last run time at the start of the file
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file.write(f"> Last update on: {current_time}\n\n")
 
-        evaluation = evaluate(rel_num_of_pos, mean_rel_growth, total_rel_growth)
-        
-        # Get currency and current price
-        try:
-            comp = yf.Ticker(company)
-            current_price = comp.info.get("ask", 0)
-            currency = comp.info.get("currency", "")
-            name = comp.info.get("shortName", "")
-            sector = comp.info.get("sector", "")
-        except:
-            # Fallback if comp.info doesn't work
-            data = yf.download(tickers=company)
-            x_current = data["Open"]
-            current_price = x_current[-1] if not x_current.empty else 0
-            currency, name, sector = "", "", ""
+    # Loop through each company and date configuration
+    for config in date_configs:
+        start_date = config["start_date"]
+        end_date = config["end_date"]
 
-        sys.stdout = sys.__stdout__  # Restore stdout
+        for company_config in company_configs:
+            key = company_config["key"]
+            companies = company_config["companies"]
 
-        catched_data = [evaluation, rel_num_of_pos, nettoGrowth, mean_rel_growth, total_rel_growth, current_price]
+            list_of_all_catched_data = []
+            companies = list(dict.fromkeys(companies))  # Remove duplicate companies
+            nmb_of_comp = len(companies)
+            remained_nmb_of_comp = nmb_of_comp
 
-        # Remove None from eval metrics and comp infos
-        catched_data = [0 if v is None else v for v in catched_data]
-        catched_data = [company, np.array(catched_data), currency, name, sector]
-        catched_data = [0 if v is None else v for v in catched_data]
-        
-        list_of_all_catched_data.append(catched_data)
-        
-        # Update the loading bar
-        os.system('clear')
-        IncrementalBar.next()
-        print("\n CPU usage: {:}%".format(psutil.cpu_percent()))
-        remained_nmb_of_comp -= 1
-        printTime(remained_nmb_of_comp * myTimer.stop())
-    
-    except Exception as e:
-        error_list.append(company)  # Append to error list if there's an issue
-        os.system('clear')
-        IncrementalBar.next()
+            os.system('clear')  # Clear the terminal screen
+            incrementalBar = IncrementalBar('Processing', max=nmb_of_comp)  # Initialize a progress bar
 
-IncrementalBar.finish()
+            # Loop through each company to download and process its data
+            for company in companies:
+                myTimer.start()  # Start the timer for the company
+                try:
+                    sys.stdout = open(os.devnull, "w")  # Suppress stdout
+                    data = yf.download(
+                        tickers=company,
+                        start=start_date,
+                        end=end_date,
+                        interval="1d",
+                        group_by='ticker'
+                    )
+                    
+                    x = data["Adj Close"]
+                    x = x[np.logical_not(np.isnan(x))]
+                    x = np.array(x)
 
-# Sort the list of companies by their evaluation value
-list_of_all_catched_data.sort(key=lambda x: x[1][0], reverse=True)
+                    diffs = np.concatenate(([0], np.diff(x)))           # daily price difference 
+                    num_of_pos = np.count_nonzero(diffs > 0)            # number of bullish days
+                    nettoGrowth = x[-1] - x[0]
+                    rel_num_of_pos = num_of_pos / len(diffs)            # relative number of bullish days to total number of days
+                    mean_rel_growth = np.mean(diffs[1:] / x[:-1])       # difference of closed price btw two days devided by closed of first day
+                    total_rel_growth = nettoGrowth / x[0]               # total relative change
 
-# Print the sorted list
-print_the_list(list_of_all_catched_data)
+                    evaluation = evaluate(rel_num_of_pos, mean_rel_growth, total_rel_growth)
+                    
+                    try:
+                        comp = yf.Ticker(company)
+                        current_price = comp.info.get("ask", 0)
+                        currency = comp.info.get("currency", "")
+                        name = comp.info.get("shortName", "")
+                        sector = comp.info.get("sector", "")
+                    except:
+                        data = yf.download(tickers=company)
+                        x_current = data["Open"]
+                        current_price = x_current[-1] if not x_current.empty else 0
+                        currency, name, sector = "", "", ""
+
+                    sys.stdout = sys.__stdout__  # Restore stdout
+
+                    catched_data = [evaluation, rel_num_of_pos, nettoGrowth, mean_rel_growth, total_rel_growth, current_price]
+                    catched_data = [0 if v is None else v for v in catched_data]
+                    catched_data = [company, np.array(catched_data), currency, name, sector]
+                    catched_data = [0 if v is None else v for v in catched_data]
+                    
+                    list_of_all_catched_data.append(catched_data)
+                    
+                    os.system('clear')
+                    incrementalBar.next()
+                    print("\n CPU usage: {:}%".format(psutil.cpu_percent()))
+                    remained_nmb_of_comp -= 1
+                    printTime(remained_nmb_of_comp * myTimer.stop())
+                
+                except Exception as e:
+                    error_list.append(company)
+                    os.system('clear')
+                    incrementalBar.next()
+
+            incrementalBar.finish()
+
+            # Sort the list of companies by their evaluation value
+            list_of_all_catched_data.sort(key=lambda x: x[1][0], reverse=True)
+
+            # Generate the markdown table for the current config
+            table_md = print_the_list(list_of_all_catched_data, start_date, end_date, key)
+
+            # Append the result to the markdown file
+            file.write(table_md + "\n\n")
 
 # Print the companies that had errors
 if error_list:
